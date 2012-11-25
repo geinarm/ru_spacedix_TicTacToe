d43888902eab9968d8776e2ae3322a0d5659e201
hjorleifurh <hjorleifurh@gmail.com>
2012-11-25 15:24:08 +0000

    Fixed conflict

--- src/include/Player.h
+++ src/include/Player.h
@@ -6,31 +6,18 @@ using namespace std;
 
 class Player
 {
-<<<<<<< HEAD
-    public:
-        void setName(string playerName);
-		string getName();
-        void addWin();
-		Player();
-        ~Player();
-    private:
-		int score;
-		string name;
-=======
-public:
-    Player();
-    ~Player();
-    void setName(string playerName);
-    string getName();
-    void setId(string playerId);
-    void addScore(int s);
-    void addScore();
-    int getScore();
-private:
-    int score;
-    string name;
-    string id;
->>>>>>> a63c3c5bf43190d6058b5d55dd8dff29447bf80e
+	public:
+   		Player();
+    	~Player();
+    	void setName(string playerName);
+    	string getName();
+    	void addScore(int s);
+    	void addScore();
+    	int getScore();
+	private:
+    	int score;
+    	string name;
+    	string id;
 };
 
 #endif // PLAYER_H
